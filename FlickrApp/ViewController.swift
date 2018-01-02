7842997e4e042926d3b5eed98fc999f2f91f24b9
Alper Akinci <alper.akinci@adastra.one>
2018-01-02 16:19:27 +0100

    Initial Commit

---
+++ FlickrApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrApp
+//
+//  Created by Alper Akinci on 02/01/2018.
+//  Copyright © 2018 Alper Akinci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
